e5df8ec08f3b5009126f832f3e333548357bdb67
kentoespdam <kentoes.pdam@gmail.com>
2024-06-10 08:42:00 +0700

    config setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "github.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
+	i18n: {
+		locales: ["id_ID"],
+		defaultLocale: "id_ID",
+	},
+}
 
-export default nextConfig;
+export default nextConfig
