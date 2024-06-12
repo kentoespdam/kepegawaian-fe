dd43e0f28cf0bf8523d19e844c5da5d389e9612f
kentoespdam <kentoes.pdam@gmail.com>
2024-06-12 08:37:19 +0700

    fix template content width

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -5,11 +5,11 @@ const MainTemplate = ({ children }: ChildrenNode) => {
     return (
         <div className="grid grid-cols-1 gap-2 mx-auto">
             <TopBarComponent />
-            <div className="grid grid-cols-1 gap-4 sm:gap-4 px-4">
-                <main className="max-w-full">{children}</main>
+            <div className="grid grid-cols-1 gap-4 sm:gap-2 px-4 sm:px-2">
+                <main className="max-w-full lg:mx-2">{children}</main>
             </div>
             <footer className="fixed bottom-0 w-full text-center">
-                &copy; Perumdam Tirta Satria
+                &copy; Perumdam Tirta Satria 2024
             </footer>
         </div>
     );
