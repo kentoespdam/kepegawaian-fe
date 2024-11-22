c8c50c08f329a527dc8f47aee0a501a36c9a708e
kentoespdam <kentoes.pdam@gmail.com>
2024-11-22 09:18:43 +0700

    add alasan berhenti

--- src/components/master/alasan-berhenti/table.tsx
+++ src/components/master/alasan-berhenti/table.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { alasanBerhentiTableColumns, type AlasanBerhenti } from "@_types/master/alasan_berhenti";
+import {
+	alasanBerhentiTableColumns,
+	type AlasanBerhenti,
+} from "@_types/master/alasan_berhenti";
 import TableHeadBuilder from "@components/builder/table/head";
 import LoadingTable from "@components/builder/table/loading";
 import { Table } from "@components/ui/table";
@@ -8,12 +11,13 @@ import { getPageData } from "@helpers/action";
 import { useQuery } from "@tanstack/react-query";
 import { useSearchParams } from "next/navigation";
 import AlasanBerhentiTableBody from "./body";
+import PaginationBuilder from "@components/builder/table/pagination";
 
 const AlasanBerhentiTable = () => {
 	const searchParams = useSearchParams();
 	const search = new URLSearchParams(searchParams);
 
-	const query = useQuery({
+	const { data, isLoading, isSuccess, error } = useQuery({
 		queryKey: ["alasan_berhenti", search.toString()],
 		queryFn: () =>
 			getPageData<AlasanBerhenti>({
@@ -26,18 +30,19 @@ const AlasanBerhentiTable = () => {
 		<div className="rounded-md">
 			<Table>
 				<TableHeadBuilder columns={alasanBerhentiTableColumns} />
-                {query.isSuccess && query.data.content.length > 0 ? (
-                    <AlasanBerhentiTableBody data={query.data} />
-                ) : (
-                    <LoadingTable
-                        isLoading={query.isLoading}
-                        error={query.error?.message}
-                        isSuccess={query.isSuccess}
-                        isEmpty={query.data?.content.length === 0}
-                        columns={alasanBerhentiTableColumns}
-                    />
-                )}
+				{isSuccess && data.content.length > 0 ? (
+					<AlasanBerhentiTableBody data={data} />
+				) : (
+					<LoadingTable
+						isLoading={isLoading}
+						error={error?.message}
+						isSuccess={isSuccess}
+						isEmpty={data?.content.length === 0}
+						columns={alasanBerhentiTableColumns}
+					/>
+				)}
 			</Table>
+			<PaginationBuilder data={data} />
 		</div>
 	);
 };
