4f914f4c7589542d2b096bd373f93617150996ec
Bagus Pdam <kentoes.pdam@gmail.com>
2024-12-03 11:18:22 +0700

    add delay

--- src/components/builder/tooltip.tsx
+++ src/components/builder/tooltip.tsx
@@ -5,11 +5,12 @@ type TooltipProps = {
     text: string;
     children: React.ReactNode;
     className?: string;
+    delayDuration?: number
 };
 
-const TooltipBuilder = ({ text, children, className }: TooltipProps) => (
-    <TooltipProvider delayDuration={0}>
-        <Tooltip delayDuration={0}>
+const TooltipBuilder = ({ text, children, className, delayDuration=0 }: TooltipProps) => (
+    <TooltipProvider delayDuration={delayDuration}>
+        <Tooltip delayDuration={delayDuration}>
             <TooltipTrigger asChild>{children}</TooltipTrigger>
             <TooltipContent className={cn("text-primary-foreground shadow-lg border border-primary-foreground",className)}>
                 <p>{text}</p>
