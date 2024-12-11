a78dde319d44c695a13795d59daf95e9bdc1820c
Bagus Pdam <kentoes.pdam@gmail.com>
2024-12-11 11:52:11 +0700

    remove unused import on table head builder

--- src/components/builder/table/head.tsx
+++ src/components/builder/table/head.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { TableHead, TableHeader, TableRow } from "@components/ui/table";
 import type { CustomColumnDef } from "@_types/index";
+import { TableHead, TableHeader, TableRow } from "@components/ui/table";
 import { cn } from "@lib/utils";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { use, useState } from "react";
-import { routeModule } from "next/dist/build/templates/app-page";
 import { ArrowDownIcon, ArrowUpDownIcon, ArrowUpIcon } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 type TableHeadBuilderProps = {
 	columns: CustomColumnDef[];
