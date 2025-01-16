40b2e78c38e8f2063d8e039d1c5cca776f8b2802
Bagus Pdam <kentoes.pdam@gmail.com>
2025-01-16 15:36:07 +0700

    optimize pegawai

--- src/app/kepegawaian/data_pegawai/form.tsx
+++ src/app/kepegawaian/data_pegawai/form.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { JenisKelamin } from "@_types/enums/jenisKelamin";
 import { PegawaiSchema, type PegawaiDetail } from "@_types/pegawai";
 import type { Biodata } from "@_types/profil/biodata";
 import SelectStatusPegawaiZod from "@components/form/zod/status-pegawai";
@@ -9,8 +8,9 @@ import PegawaiBiodataComponent from "@components/kepegawaian/data_pegawai/add/bi
 import PegawaiDetailComponent from "@components/kepegawaian/data_pegawai/add/detail_pegawai";
 import { Form } from "@components/ui/form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useAddBiodataStore } from "@store/kepegawaian/biodata/add-store";
+import { usePegawaiStore } from "@store/kepegawaian/data_pegawai/pegawai";
 import { useGlobalMutation } from "@store/query-store";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { saveKepegawaian } from "./action";
 
@@ -19,65 +19,18 @@ type PegawaiFormProps = {
 	pegawai?: PegawaiDetail;
 };
 const PegawaiForm = ({ biodata, pegawai }: PegawaiFormProps) => {
-	const biodataStore = useAddBiodataStore();
+	const { defaultValues, setDefaultValues } = usePegawaiStore((state) => ({
+		defaultValues: state.defaultValues,
+		setDefaultValues: state.setDefaultValues
+	}))
 
 	const form = useForm<PegawaiSchema>({
 		resolver: zodResolver(PegawaiSchema),
-		defaultValues: {
-			statusPegawai: biodataStore.statusPegawai,
-			nik: "",
-			nama: "",
-			jenisKelamin: JenisKelamin.Values.LAKI_LAKI,
-			golonganDarah: "",
-			statusKawin: "",
-			agama: "",
-			tempatLahir: "",
-			tanggalLahir: "",
-			ibuKandung: "",
-			telp: "",
-			pendidikanTerakhirId: 0,
-			alamat: "",
-			notes: "",
-			nipam: "",
-			statusKerja:
-				biodataStore.statusPegawai !== "NON_PEGAWAI" ? "KARYAWAN_AKTIF" : "",
-			organisasiId: 0,
-			jabatanId: 0,
-			profesiId: 0,
-			golonganId: 0,
-			nomorSk: "",
-			tanggalSk: "",
-			tmtKontrakSelesai: "",
-			gajiPokok: 0,
-		},
-		values: {
-			statusPegawai: biodataStore.statusPegawai,
-			nik: biodata?.nik ?? "",
-			nama: biodata?.nama ?? "",
-			jenisKelamin: biodata?.jenisKelamin ?? JenisKelamin.Values.LAKI_LAKI,
-			golonganDarah: biodata?.golonganDarah ?? "",
-			statusKawin: biodata?.statusKawin ?? "",
-			agama: biodata?.agama ?? "",
-			tempatLahir: biodata?.tempatLahir ?? "",
-			tanggalLahir: biodata?.tanggalLahir ?? "",
-			ibuKandung: biodata?.ibuKandung ?? "",
-			telp: biodata?.telp ?? "",
-			pendidikanTerakhirId: biodata?.pendidikanTerakhir.id ?? 0,
-			alamat: biodata?.alamat ?? "",
-			notes: biodata?.notes ?? "",
-			nipam: pegawai?.nipam ?? "",
-			statusKerja:
-				biodataStore.statusPegawai !== "NON_PEGAWAI" ? "KARYAWAN_AKTIF" : "",
-			organisasiId: pegawai?.organisasi.id ?? 0,
-			jabatanId: pegawai?.jabatan.id ?? 0,
-			profesiId: pegawai?.profesi.id ?? 0,
-			golonganId: pegawai?.golongan.id ?? 0,
-			nomorSk: pegawai?.nomorSk ?? "",
-			tanggalSk: pegawai?.tanggalSk ?? "",
-			tmtKontrakSelesai: pegawai?.tmtKontrakSelesai ?? "",
-			gajiPokok: 0,
-		},
+		defaultValues: defaultValues,
+		values: defaultValues,
 	});
+
+	const statusPegawai = form.watch("statusPegawai")
 
 	const mutation = useGlobalMutation({
 		mutationFunction: saveKepegawaian,
@@ -93,6 +46,8 @@ const PegawaiForm = ({ biodata, pegawai }: PegawaiFormProps) => {
 		console.log(values)
 	};
 
+	useEffect(() => setDefaultValues(pegawai, biodata), [setDefaultValues, pegawai, biodata])
+
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
@@ -101,8 +56,7 @@ const PegawaiForm = ({ biodata, pegawai }: PegawaiFormProps) => {
 					label="Status Pegawai"
 					form={form}
 				/>
-				{biodataStore.statusPegawai !== "" &&
-				biodataStore.statusPegawai !== "NON_PEGAWAI" ? (
+				{statusPegawai !== "NON_PEGAWAI" ? (
 					<PegawaiDetailComponent form={form} />
 				) : null}
 
