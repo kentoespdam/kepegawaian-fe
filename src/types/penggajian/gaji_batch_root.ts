e73eaa398e4e3fd3215435e7fb6fb8a4e8bc9f65
Bagus Pdam <kentoes.pdam@gmail.com>
2025-02-24 10:11:36 +0700

    change field tgl to tanggal

---
+++ src/types/penggajian/gaji_batch_root.ts
@@ -0,0 +1,77 @@
+import { z } from "zod";
+import { ACCEPTED_FILE_TYPES, type CustomColumnDef, MAX_UPLOAD_SIZE } from "..";
+
+export interface GajiBatchRootErrorLogs {
+	id: number;
+	batchId: string;
+	nipam: string;
+	nama: string;
+	notes: string;
+}
+export interface GajiBatchRoot {
+	batchId: string;
+	periode: string;
+	status: string;
+	totalPegawai: number;
+	tanggalProses: string;
+	diProsesOleh: string;
+	jabatanPemroses: string;
+	tanggalVerifikasiTahap1: string;
+	diVerifikasiOlehTahap1: string;
+	jabatanVerifikasiTahap1: string;
+	tanggalVerifikasiTahap2: string;
+	diVerifikasiOlehTahap2: string;
+	jabatanVerifikasiTahap2: string;
+	tanggalPersetujuan: string;
+	diSetujuiOleh: string;
+	jabatanPersetujuan: string;
+	errorLogs: GajiBatchRootErrorLogs[];
+	notes: string;
+	mimeType: string;
+	fileName: string;
+}
+
+export const GajiBatchRootSchema = z.object({
+	tahun: z.string(),
+	bulan: z.string(),
+	diProsesOleh: z.string(),
+	jabatanPemroses: z.string(),
+	fileName: z
+		.any()
+		.refine(
+			(files) => Array.from(files).every((file) => file instanceof File),
+			"File wajib diisi",
+		)
+		.refine(
+			(files) =>
+				Array.from(files).every(
+					(file) =>
+						file instanceof File && ACCEPTED_FILE_TYPES.includes(file.type),
+				),
+			"Invalid file type",
+		)
+		.refine(
+			(files) =>
+				Array.from(files).every(
+					(file) => file instanceof File && file.size <= MAX_UPLOAD_SIZE,
+				),
+			"Maks File Upload 10 MB",
+		)
+		.optional(),
+});
+
+export type GajiBatchRootSchema = z.infer<typeof GajiBatchRootSchema>;
+
+export const gajiBatchRootColumns: CustomColumnDef[] = [
+	{ id: "urut", label: "No" },
+	{ id: "aksi", label: "Aksi" },
+	{ id: "periode", label: "Periode", search: true, searchType: "text" },
+	{ id: "batchId", label: "Batch ID", search: true, searchType: "text" },
+	{ id: "status", label: "Status" },
+	{ id: "tanggalProses", label: "Tanggal Proses" },
+	{ id: "totalPegawai", label: "Total Pegawai" },
+	{ id: "tanggalVerifikasiTahap1", label: "Tanggal Verifikasi Tahap 1" },
+	{ id: "tanggalVerifikasiTahap2", label: "Tanggal Verifikasi Tahap 2" },
+	{ id: "tanggalPersetujuan", label: "Tanggal Persetujuan" },
+	{ id: "notes", label: "Notes" },
+];
