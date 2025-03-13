b219fe34849d52904a3e79920bea1c49fdb3a75f
Bagus Pdam <kentoes.pdam@gmail.com>
2025-03-13 09:33:20 +0700

    persetujuan akhir min send kirim slip gaji

--- src/components/penggajian/approval/index.tsx
+++ src/components/penggajian/approval/index.tsx
@@ -3,22 +3,22 @@
 import type { Organisasi } from "@_types/master/organisasi";
 import type { GajiBatchMaster } from "@_types/penggajian/gaji_batch_master";
 import { verifPhase2Columns } from "@_types/penggajian/verif_phase2";
-import SearchBuilder from "@components/builder/search";
 import TableHeadBuilder from "@components/builder/table/head";
 import LoadingTable from "@components/builder/table/loading";
 import { getNamaBulan } from "@helpers/tanggal";
 import { FileSpreadsheetIcon } from "lucide-react";
 import { useSearchParams } from "next/navigation";
-import VerifPhase2TableBody from "../gaji_batch_master/verif_phase_2/table.body";
+import ApprovalTableBody from "../gaji_batch_master/approval/table.body";
+import SearchBuilder from "@components/builder/search";
 
-interface VerifPhase2ComponentProps {
+interface ApprovalComponentProps {
 	organisasiList: Organisasi[];
 	gajiBatchMasters?: GajiBatchMaster[];
 }
-const VerifPhase2Component = ({
+const ApprovalComponent = ({
 	organisasiList,
 	gajiBatchMasters,
-}: VerifPhase2ComponentProps) => {
+}: ApprovalComponentProps) => {
 	const searchParams = useSearchParams();
 	const periode = searchParams.get("periode");
 	const tahun = periode?.substring(0, 4);
@@ -26,24 +26,22 @@ const VerifPhase2Component = ({
 
 	return (
 		<div className="grid gap-2 pr-4">
-			<div>
-				<h2 className="flex">
-					<FileSpreadsheetIcon className="w-5 h-5 mr-1" />
-					Tambah Komponen Gaji [Periode{" "}
-					<span className="ml-2 text-primary">
-						{getNamaBulan(Number(bulan))} {tahun}
-					</span>
-					]
-				</h2>
-			</div>
-			{/* <SearchBuilder columns={verifPhase2Columns} /> */}
-			<div className="block max-h-[70vh] min-h-[350px] overflow-y-auto">
+			<h2 className="flex">
+				<FileSpreadsheetIcon className="w-5 h-5 mr-1" />
+				Tambah Komponen Gaji [Periode{" "}
+				<span className="ml-2 text-primary">
+					{getNamaBulan(Number(bulan))} {tahun}
+				</span>
+				]
+			</h2>
+			<SearchBuilder columns={verifPhase2Columns} />
+			<div className="block max-h-[80vh] min-h-[350px] overflow-y-auto">
 				<table className="w-full">
 					<TableHeadBuilder columns={verifPhase2Columns} />
 					{!gajiBatchMasters ? (
 						<LoadingTable columns={verifPhase2Columns} isLoading={false} />
 					) : (
-						<VerifPhase2TableBody
+						<ApprovalTableBody
 							organisasiList={organisasiList}
 							gajiBatchMasters={gajiBatchMasters}
 						/>
@@ -54,4 +52,4 @@ const VerifPhase2Component = ({
 	);
 };
 
-export default VerifPhase2Component;
+export default ApprovalComponent;
--- src/components/penggajian/gaji_batch_master_process/table.body.tsx
+++ src/components/penggajian/gaji_batch_master_process/table.body.tsx
@@ -1,38 +1,60 @@
-import { JENIS_GAJI, getKeyJenisGaji, type JenisGaji } from "@_types/enums/jenis_gaji";
-import { gajiBatchMasterProsesColumns, type GajiBatchMasterProses } from "@_types/gaji_batch_master_process";
-import { TableBody, TableCell, TableFooter, TableRow } from "@components/ui/table";
+import {
+    type JenisGaji,
+    getKeyJenisGaji
+} from "@_types/enums/jenis_gaji";
+import {
+    type GajiBatchMasterProses,
+    gajiBatchMasterProsesColumns,
+} from "@_types/gaji_batch_master_process";
+import {
+    TableBody,
+    TableCell,
+    TableFooter,
+    TableRow,
+} from "@components/ui/table";
 import { rupiah } from "@helpers/number";
 
 interface GajiBatchMasterProsesTableBodyProps {
-    data: GajiBatchMasterProses[],
-    jenisGaji: JenisGaji
+	data: GajiBatchMasterProses[];
+	jenisGaji: JenisGaji;
 }
-const GajiBatchMasterProsesTableBody = ({ data, jenisGaji }: GajiBatchMasterProsesTableBodyProps) => {
-    const filtered = data.filter(item => item.jenisGaji === getKeyJenisGaji(jenisGaji))
-    const penghasilan = data.find(item => item.kode === (jenisGaji === JENIS_GAJI.PEMASUKAN ? "PENGHASILAN_KOTOR" : "TOTAL_POTONGAN"))
-    let urut = 1;
-    return (
-        <>
-            <TableBody>
-                {filtered.map(item => (
-                    <TableRow key={item.id}>
-                        <TableCell className="border-x" align="right" width={45}>{urut++}</TableCell>
-                        <TableCell className="border-x">{item.nama}</TableCell>
-                        <TableCell className="border-x" align="right">{rupiah(item.nilai)}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-            <TableFooter>
-                <TableRow>
-                    <TableCell colSpan={gajiBatchMasterProsesColumns.length}
-                        className="border"
-                        align="right">
-                        <span className="font-bold">{rupiah(penghasilan?.nilai ?? 0)}</span>
-                    </TableCell>
-                </TableRow>
-            </TableFooter>
-        </>
-    );
-}
+const GajiBatchMasterProsesTableBody = ({
+	data,
+	jenisGaji,
+}: GajiBatchMasterProsesTableBodyProps) => {
+	const filtered = data.filter(
+		(item) => item.jenisGaji === getKeyJenisGaji(jenisGaji),
+	);
+	const penghasilan = filtered.reduce((total, item) => total + item.nilai, 0);
+	let urut = 1;
+	return (
+		<>
+			<TableBody>
+				{filtered.map((item) => (
+					<TableRow key={item.id}>
+						<TableCell className="border-x" align="right" width={45}>
+							{urut++}
+						</TableCell>
+						<TableCell className="border-x">{item.nama}</TableCell>
+						<TableCell className="border-x" align="right">
+							{rupiah(item.nilai)}
+						</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+			<TableFooter>
+				<TableRow>
+					<TableCell
+						colSpan={gajiBatchMasterProsesColumns.length}
+						className="border"
+						align="right"
+					>
+						<span className="font-bold">{rupiah(penghasilan)}</span>
+					</TableCell>
+				</TableRow>
+			</TableFooter>
+		</>
+	);
+};
 
 export default GajiBatchMasterProsesTableBody;
