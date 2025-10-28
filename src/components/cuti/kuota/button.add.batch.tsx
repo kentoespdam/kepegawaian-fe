3fe63fd9351c24c159608c6e40009cdc656d721a
Kentoes PDAM <kentoes.pdam@gmail.com>
2025-10-28 14:52:34 +0700

    optimize kuota cuti

--- src/components/cuti/kuota/button.add.batch.tsx
+++ src/components/cuti/kuota/button.add.batch.tsx
@@ -1,21 +1,26 @@
-"use client";
+"use client"
 
-import TooltipBuilder from "@components/builder/tooltip";
-import { Button } from "@components/ui/button";
-import { useCutiKuotaStore } from "@store/cuti/kuota";
-import { UploadIcon } from "lucide-react";
+import TooltipBuilder from "@components/builder/tooltip"
+import { Button } from "@components/ui/button"
+import { useCutiKuotaStore } from "@store/cuti/kuota"
+import { UploadIcon } from "lucide-react"
+import { useCallback } from "react"
 
 const AddKuotaCutiBatchButton = () => {
 	const { setOpenBatch } = useCutiKuotaStore((state) => ({
 		setOpenBatch: state.setOpenBatch,
-	}));
+	}))
+
+	const uploadHandler = useCallback(() => setOpenBatch(true), [setOpenBatch])
 
 	return (
-		<TooltipBuilder text="Tambah Batch Kuota Cuti" delayDuration={100} className="bg-warning text-warning-foreground">
+		<TooltipBuilder
+			text="Tambah Batch Kuota Cuti"
+			delayDuration={100}
+			className="bg-warning text-warning-foreground"
+		>
 			<Button
-				onClick={() => {
-					setOpenBatch(true);
-				}}
+				onClick={uploadHandler}
 				variant={"ghost"}
 				className="text-warning hover:opacity-75"
 				size={"icon"}
@@ -23,7 +28,7 @@ const AddKuotaCutiBatchButton = () => {
 				<UploadIcon className=" h-5 w-5" />
 			</Button>
 		</TooltipBuilder>
-	);
-};
+	)
+}
 
-export default AddKuotaCutiBatchButton;
+export default AddKuotaCutiBatchButton
