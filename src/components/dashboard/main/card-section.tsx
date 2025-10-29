d32233aeb9aed5d175aa0485ee9b5d63d0eea5ac
Kentoes PDAM <kentoes.pdam@gmail.com>
2025-10-29 10:55:35 +0700

    Refactor dashboard components and add new cards for Kenaikan Berkala and Lepas Tanggungan Anak

--- src/components/dashboard/main/card-section.tsx
+++ src/components/dashboard/main/card-section.tsx
@@ -1,10 +1,24 @@
 import type { PegawaiDetail } from "@_types/pegawai";
+import type { User } from "@_types/user";
+import DashboardKenaikanBerkalaCard from "./card-kgb";
+import DashboardLepasTanggunganAnakCard from "./card-lta";
 import DashboardCutiCard from "./cuti-card";
 
-const DashboardCardSection = ({ pegawai }: { pegawai: PegawaiDetail }) => {
+type DashboardCardSectionProps = {
+	user: User;
+	pegawai: PegawaiDetail;
+};
+const DashboardCardSection = ({ user,pegawai }: DashboardCardSectionProps) => {
+	const roles = user.prefs.roles;
 	return (
 		<div className="grid gap-2 lg:grid-cols-4 sm:grid-cols-2">
 			<DashboardCutiCard pegawai={pegawai} />
+			{roles?.includes("ADMIN") || roles?.includes("HRD") ? (
+				<>
+					<DashboardKenaikanBerkalaCard />
+					<DashboardLepasTanggunganAnakCard />
+				</>
+			) : null}
 		</div>
 	);
 };
